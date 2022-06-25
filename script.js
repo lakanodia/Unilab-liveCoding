a1d7d50af28d67807d6fdb5a8492ddbe706fdde6
Laka Nodia <laka.nodia@gmail.com>
2022-06-25 12:01:38 +0400

    Add function

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 
-let data = [
-    {
-        id: 1,
-        title: 'HTML'
-    },
-    {
-        id: 2,
-        title: 'CSS'
-    },
-    {
-        id: 3,
-        title: 'Javascript'
-    },
-]
-
-
 let htmlButton = document.getElementById('button-html');
 let cssButton = document.getElementById('button-css');
 let javascriptButton = document.getElementById('button-javascript');
 
 
-htmlButton.addEventListener('click', function(item) {
+
+htmlButton.addEventListener('click', function(){
     let post = document.getElementById('post');
     let pTag = document.createElement('p');
     post.classList.add('active');
     pTag.classList.add('title');
-    pTag.textContent('HTML');
+    pTag.textContent = 'HTML';
     post.appendChild(pTag);
 });
+
+cssButton.addEventListener('click', function(){
+    let postCss = document.getElementById('post-css');
+    let pTag = document.createElement('p');
+    postCss.classList.add('active');
+    pTag.classList.add('title');
+    pTag.textContent = 'CSS';
+    postCss.appendChild(pTag);
+});
+
+javascriptButton.addEventListener('click', function(){
+    let postJava = document.getElementById('post-javascript');
+    let pTag = document.createElement('p');
+    postJava.classList.add('active');
+    pTag.classList.add('title');
+    pTag.textContent = 'Javascript';
+    postJava.appendChild(pTag);
+});
+
+
+
 
 
 
